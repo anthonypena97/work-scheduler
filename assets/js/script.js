fefa3bb4f757f2381449d9188ffe8efb0770bd26
Anthony Pena <apena5@alumni.jh.edu>
2021-08-19 22:36:07 -0400

    appending each hour to block container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
 var today = moment().format("[Today is] dddd MMMM Do[,] YYYY");
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"]
+
+populateHours = function () {
+
+    $.each(hours, function (i) {
+
+        let hourBlock = $('<div>').addClass("time-block").text(hours[i]);
+
+        $('.container').append(hourBlock);
+
+    })
+
+};
 
 updateToday = function () {
     $('#currentDay').text(today);
-}
-
-console.log(today);
+};
 
 updateToday();
+
+populateHours();
